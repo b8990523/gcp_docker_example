3497e4907ee922a03de21077ebfb61b63fc267c9
b8990523 <56726298+b8990523@users.noreply.github.com>
2024-12-18 17:21:09 +0800

    Update main.py

--- App Engine/test-flask/main.py
+++ App Engine/test-flask/main.py
@@ -1,9 +1,10 @@
+# flask 是 python 最基本的一個網頁框架
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # 當使用者輸入這個路徑後回傳下列內容
 def hello():
     return "Hello, World! 2024/7/22"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #執行部分從app run開始
