c6fab3f332e856b7f25a6376b9d1fe7dc608c06d
b8990523 <56726298+b8990523@users.noreply.github.com>
2024-12-18 16:38:24 +0800

    Create main.tf

---
+++ test_terraform/test2/main.tf
@@ -0,0 +1,38 @@
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "e2-micro"
+  zone         = "asia-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240726"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+resource "google_compute_instance" "example2" {
+  name         = "example2-instance"
+  machine_type = "e2-micro"
+  zone         = "asia-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240726"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
