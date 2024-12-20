89dab5bb9717546a8f849972bc7330193c42c2d5
b8990523 <56726298+b8990523@users.noreply.github.com>
2024-12-20 17:30:18 +0800

    Update client.py

--- App Engine/test-iris/client.py
+++ App Engine/test-iris/client.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
 # Change the value of experience that you want to test
-url = 'https://iris-predict-dot-mygcp-436602.de.r.appspot.com/api'   # 改成 127.0.0.1
+# 在部屬前先用 url=http://127.0.0.1:8080/api
+url = 'https://iris-predict-dot-light-team-436602-j3.de.r.appspot.com/api'   
 feature = [[5.8, 4.0, 1.2, 0.2]]
 labels ={
   0: "setosa",
